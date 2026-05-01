dc0794ea318fa558446aa8a85a364137582bf721
Kishalaya Bhattacharjee <Kishalaya1@yahoo.com>
2026-05-01 19:51:24 +0530

    2 types of delegate example

---
+++ DelegatesDemo/DelegatesExample.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
+
+namespace Entities
+{
+    public class DelegatesExample
+    {
+        #region SimpleDelegate 
+        public delegate void StartEntertainmentContent(string channelName);
+        public static void StartTvChannel(string channelName)
+        {
+            Console.WriteLine("Started TV Channel : " + channelName);
+        }
+        public static void SimpleDelegate()
+        {
+            Console.WriteLine("Simple Delegate Example demo start ----");
+            // Define a delegate
+            StartEntertainmentContent simpleDelegate = StartTvChannel;
+            // Use the delegate
+            simpleDelegate("TNT Movie");
+            Console.WriteLine("Simple Delegate Example demo end ----");
+        }
+        #endregion
+
+        #region MulticastDelegate
+        public delegate void StartTVChannels();
+        static void ChannelA() => Console.WriteLine("El TV channel started");
+        static void ChannelB() => Console.WriteLine("MTV channel started");
+        static void ChannelC() => Console.WriteLine("Channel V started");
+        public static void MultiCastDelegate()
+        {
+            Console.WriteLine("Multicast Delegate Example demo start ----");
+            //first assign first method to the delegate
+            StartTVChannels multiTipleChannels = ChannelA;
+            // adding multiple methods to the same delegate
+            multiTipleChannels += ChannelB;
+            multiTipleChannels+= ChannelC;
+            /// Now when we invoke the delegate, it will call all the methods assigned to it
+            multiTipleChannels();
+            Console.WriteLine("Multicast Delegate Example demo end ----");
+        }
+        
+        
+        #endregion
+    }
+}
--- DelegatesDemo/Program.cs
+++ DelegatesDemo/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using Entities;
+
+Console.WriteLine("Hello, World!");
+Demo();
+static void Demo()
+{
+    
+    DelegatesExample.SimpleDelegate();
+    DelegatesExample.MultiCastDelegate();
+}
